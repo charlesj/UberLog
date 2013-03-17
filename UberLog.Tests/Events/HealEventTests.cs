67cfa1d687c8043726db61a4e7d201d6d2663173
Josh Charles <josh.charles@gmail.com>
2013-03-17 08:38:15 -0700

    Added HealEvent Tests.  Implemented.

---
+++ UberLog.Tests/Events/HealEventTests.cs
@@ -0,0 +1,101 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HealEventTests.cs" company="Josh Charles">
+//   Licensed under the GPL.
+// </copyright>
+// <summary>
+//   Defines the HealEventTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace UberLog.Tests.Events
+{
+	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Linq;
+
+	using UberLog.Events;
+
+	using Xunit;
+	using Xunit.Extensions;
+
+	[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
+	public class HealEventTests
+	{
+		[Theory, PropertyData("SteamIds")]
+		public void SetsSteamId(HealEvent evt, string steamId)
+		{
+			Assert.Equal(steamId, evt.Player.SteamId);
+		}
+
+		[Theory, PropertyData("TargetSteamIds")]
+		public void SetsTargetSteamId(HealEvent evt, string targetSteamId)
+		{
+			Assert.Equal(targetSteamId, evt.TargetPlayer.SteamId);
+		}
+
+		[Theory, PropertyData("Amounts")]
+		public void SetsAmounts(HealEvent evt, int amount)
+		{
+			Assert.Equal(amount, evt.Amount);
+		}
+
+		public static IEnumerable<object[]> SteamIds
+		{
+			get
+			{
+				return Logs.Select(l => new object[] { EventTestHelpers.BuildAndParse<HealEvent>(l.LogText), l.PlayerSteamId });
+			}
+		}
+
+		public static IEnumerable<object[]> TargetSteamIds
+		{
+			get
+			{
+				return Logs.Select(l => new object[] { EventTestHelpers.BuildAndParse<HealEvent>(l.LogText), l.TargetSteamId });
+			}
+		}
+
+		public static IEnumerable<object[]> Amounts
+		{
+			get
+			{
+				return Logs.Select(l => new object[] { EventTestHelpers.BuildAndParse<HealEvent>(l.LogText), l.Amount });
+			}
+		}
+
+		private static List<HealInfo> Logs
+		{
+			get
+			{
+				return new List<HealInfo>
+				{
+					new HealInfo
+					{
+						LogText = "L 03/04/2013 - 20:44:40: \"Schrodinger.esg<6><STEAM_0:1:26162903><Blue>\" triggered \"healed\" against \"Toast<2><STEAM_0:1:12365159><Blue>\" (healing \"26\")",
+						PlayerSteamId = "STEAM_0:1:26162903",
+						TargetSteamId = "STEAM_0:1:12365159",
+						Amount = 26
+					},
+					new HealInfo
+					{
+						LogText = "L 03/04/2013 - 20:44:54: \"Schrodinger.esg<6><STEAM_0:1:26162903><Blue>\" triggered \"healed\" against \"Shamrock.esg<8><STEAM_0:0:545976><Blue>\" (healing \"3\")",
+						PlayerSteamId = "STEAM_0:1:26162903",
+						TargetSteamId = "STEAM_0:0:545976",
+						Amount = 3
+					}
+				};
+			}
+		}
+
+		private class HealInfo
+		{
+			public string LogText { get; set; }
+
+			public string PlayerSteamId { get; set; }
+
+			public string TargetSteamId { get; set; }
+
+			public int Amount { get; set; }
+		}
+	}
+}
--- UberLog/Events/HealEvent.cs
+++ UberLog/Events/HealEvent.cs
@@ -34,11 +34,33 @@ namespace UberLog.Events
 		public override string Keystone { get; protected set; }
 
 		/// <summary>
+		/// Gets or sets the player giving the heals.
+		/// </summary>
+		public Player Player { get; set; }
+
+		/// <summary>
+		/// Gets or sets the target player receiving the heals.
+		/// </summary>
+		public Player TargetPlayer { get; set; }
+
+		/// <summary>
+		/// Gets or sets the amount healed.
+		/// </summary>
+		public int Amount { get; set; }
+
+		/// <summary>
 		/// The parse.
 		/// </summary>
 		public override void Parse()
 		{
-			throw new System.NotImplementedException();
+			var matches = this.GetMatches();
+			var playerString = matches[0].Value;
+			var targetString = matches[4].Value;
+			var amountString = matches[6].Value;
+
+			this.Player = this.PlayerHelper(playerString);
+			this.TargetPlayer = this.PlayerHelper(targetString);
+			this.Amount = int.Parse(amountString);
 		}
 	}
 }
